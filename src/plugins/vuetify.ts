3c8ddba2acbd73c5b5e9dd537ae840d223d749f3
sasha <sashazemzer@gmail.com>
2020-02-05 23:26:42 +0300

    vuetify configured

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,26 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import { ru } from 'vuetify/src/locale';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    customProperties: true,
+    icons: {
+        iconfont: 'mdi',
+    },
+    theme: {
+        themes: {
+            light: {
+                primary: '#150734',
+            },
+        },
+    },
+    options: {
+        customProperties: true,
+    },
+    lang: {
+        locales: { ru },
+        current: 'ru',
+    },
 });
