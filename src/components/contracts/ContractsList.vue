4795cc44b9f9ab02a258938674bcfee495d86ac3
sasha <sashazemzer@gmail.com>
2020-02-06 11:04:01 +0300

    work with contracts page

---
+++ src/components/contracts/ContractsList.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-container fluid>
+        <v-row>
+            <v-col>
+                <v-data-table
+                        :headers="headers"
+                        fixed-header
+                        multi-sort
+                ></v-data-table>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ContractsList extends Vue {
+    public headers = [
+        { text: 'Статус', align: 'left', sortable: false, value: 'name', width: '10%' },
+        { text: 'Договор', value: 'calories', width: '40%' },
+        { text: 'Арендатор', value: 'fat', width: '40%' },
+        { text: 'Просмотр', value: 'carbs', sortable: false },
+    ];
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/types/common.ts
@@ -0,0 +1,5 @@
+
+export class MenuItem {
+    public text: string = '';
+    public action: string = '';
+}
