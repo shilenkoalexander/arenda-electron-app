01e2be6ea24c784cf49049b15df561c4b54c36c6
sasha <sashazemzer@gmail.com>
2020-02-14 16:45:33 +0300

    added contracts page

--- src/components/contracts/ContractDetails.vue
+++ src/components/contracts/ContractDetails.vue
@@ -1,11 +1,11 @@
 <template>
     <v-container fluid class="px-10">
         <v-row>
-            <v-col class="d-flex" align-self="center" cols="8">
+            <v-col class="d-flex" align-self="center" cols="9">
                 <p class="mb-0 title grey--text">№ договора: </p>
                 <p class="mb-0 ml-1 title">{{item.contractInfo.number}}</p>
             </v-col>
-            <v-col class="d-flex" align-self="center">
+            <v-col class="d-flex justify-end" align-self="center">
                 <p class="mb-0 subtitle-1 grey--text">Статус:</p>
                 <v-icon :color="statusIconColor" size="30" class="ml-1">
                     {{statusIcon}}
@@ -15,70 +15,91 @@
         </v-row>
         <v-divider/>
 
-        <ContractDetailsItem class="mt-2" text="Вид договора:" :value="item.contractInfo.type"/>
-        <ContractDetailsItem text="Дата заключения:" :value="formatToFriendly(item.contractInfo.startDate)"/>
-        <ContractDetailsItem 
+        <ContractDetailsItem class="mt-2" text="Вид договора" :value="item.contractInfo.type"/>
+        <ContractDetailsItem text="Дата заключения" :value="formatToFriendly(item.contractInfo.startDate)"/>
+        <ContractDetailsItem
                 v-if="!isClosed"
-                text="Дата действия:"
+                text="Дата действия"
                 :value="formatToFriendly(item.contractInfo.validity)"
         />
 
         <template v-if="isClosed">
-            <ContractDetailsItem text="Дата расторжения:" :value="formatToFriendly(item.contractInfo.endDate)"/>
-            <ContractDetailsItem text="Причина расторжения:" :value="item.contractInfo.endReason"/>
+            <ContractDetailsItem text="Дата расторжения" :value="formatToFriendly(item.contractInfo.endDate)"/>
+            <ContractDetailsItem text="Причина расторжения" :value="item.contractInfo.endReason"/>
         </template>
 
-        <v-row>
-            <v-col>
-                <p class="mb-0 title grey--text">Арендатор</p>
-            </v-col>
-        </v-row>
-        <v-divider/>
-        <ContractDetailsItem class="mt-2" text="Название организации:" :value="item.contractInfo.organizationName"/>
-        <ContractDetailsItem text="Руководитель:" :value="item.contractInfo.responsiblePerson"/>
-        <ContractDetailsItem text="Юридический адрес:" :value="item.contractInfo.legalAddress"/>
-        <ContractDetailsItem text="ИНН:" :value="item.contractInfo.inn"/>
+        <ContractDetailsHeader text="Арендатор"/>
+        <ContractDetailsItem text="Название организации" :value="item.contractInfo.organizationName"/>
+        <ContractDetailsItem text="Руководитель" :value="item.contractInfo.responsiblePerson"/>
+        <ContractDetailsItem text="Юридический адрес" :value="item.contractInfo.legalAddress"/>
+        <ContractDetailsItem text="ИНН" :value="item.contractInfo.inn"/>
+
+        <ContractDetailsHeader text="Контакты"/>
+        <ContractDetailsItem
+                v-for="(contact, index) in item.contacts"
+                :key="index"
+                :text="getContactTypeValue(contact.type)"
+                :value="contact.contact"
+        />
+
+        <ContractDetailsHeader text="Активные объекты"/>
+        <ContractDetailsObjectItem
+                v-for="(object, index) in item.objectsInfo"
+                :key="object.id"
+                :order-number="index + 1"
+                :object-item="object"
+        />
     </v-container>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-import { ContractStatus, FullContractDetails, getContractStatusValue } from '@/types/contracts';
-import { getIconByStatus, getIconColorByStatus } from '@/utils/icon-utils';
-import { formatToFriendly } from '@/utils/date-utils';
-import ContractDetailsItem from '@/components/contracts/ContractDetailsItem.vue';
+    import { Component, Prop, Vue } from 'vue-property-decorator';
+    import {
+        ContractStatus,
+        FullContractDetails,
+        getContactTypeValue,
+        getContractStatusValue,
+    } from '@/types/contracts';
+    import { getIconByStatus, getIconColorByStatus } from '@/utils/icon-utils';
+    import { formatToFriendly } from '@/utils/date-utils';
+    import ContractDetailsItem from '@/components/contracts/ContractDetailsItem.vue';
+    import ContractDetailsHeader from '@/components/contracts/ContractDetailsHeader.vue';
+    import ContractDetailsObjectItem from '@/components/contracts/ContractDetailsObjectItem.vue';
 
-@Component({
-    components: {
-        ContractDetailsItem,
-    },
-})
-export default class ContractDetails extends Vue {
-    @Prop({
-        required: true,
-        type: Object,
+    @Component({
+        components: {
+            ContractDetailsItem,
+            ContractDetailsHeader,
+            ContractDetailsObjectItem,
+        },
     })
-    item!: FullContractDetails;
+    export default class ContractDetails extends Vue {
+        @Prop({
+            required: true,
+            type: Object,
+        })
+        item!: FullContractDetails;
 
-    getContractStatusValue = getContractStatusValue;
-    formatToFriendly = formatToFriendly;
+        getContractStatusValue = getContractStatusValue;
+        getContactTypeValue = getContactTypeValue;
+        formatToFriendly = formatToFriendly;
 
-    get statusIcon(): string {
-        return getIconByStatus(this.item.contractInfo.status);
+        get statusIcon(): string {
+            return getIconByStatus(this.item.contractInfo.status);
+        }
+
+        get statusIconColor(): string {
+            return getIconColorByStatus(this.item.contractInfo.status);
+        }
+
+        get status(): string {
+            return getContractStatusValue(this.item.contractInfo.status);
+        }
+
+        get isClosed() {
+            return this.item.contractInfo.status === ContractStatus.CLOSED;
+        }
     }
-
-    get statusIconColor(): string {
-        return getIconColorByStatus(this.item.contractInfo.status);
-    }
-
-    get status(): string {
-        return getContractStatusValue(this.item.contractInfo.status);
-    }
-
-    get isClosed() {
-        return this.item.contractInfo.status === ContractStatus.CLOSED;
-    }
-}
 </script>
 
 <style scoped>
---
+++ src/components/contracts/ContractDetailsHeader.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <v-row>
+            <v-col>
+                <p class="mb-0 title grey--text">{{text}}</p>
+            </v-col>
+        </v-row>
+        <v-divider class="mb-2"/>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ContractDetailsHeader extends Vue {
+    @Prop({
+        type: String,
+        required: true,
+    })
+    text!: string;
+}
+</script>
+
+<style scoped>
+
+</style>
