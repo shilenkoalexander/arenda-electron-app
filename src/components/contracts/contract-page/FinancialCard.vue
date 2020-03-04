e48c3bdb6c5e8f3c8576a83352404019aea11731
sasha <sashazemzer@gmail.com>
2020-03-05 00:09:35 +0300

    contract page

--- src/components/contracts/contract-page/FinancialCard.vue
+++ src/components/contracts/contract-page/FinancialCard.vue
@@ -1,36 +1,53 @@
 <template>
     <v-card>
         <v-container fluid>
-            <v-row>
-                <v-col cols="3">
-                    <v-text-field label="Месяц" hide-details outlined dense/>
-                </v-col>
-                <v-col cols="3" offset="6">
-                    <v-btn block color="primary lighten-1">Все операции</v-btn>
-                </v-col>
-            </v-row>
-            <v-row>
-                <v-col>
-                    <TextValueItem text="Начисление" value="314.35 р"/>
-                </v-col>
-                <v-col>
-                    <TextValueItem text="Оплата" value="314.35 р"/>
-                </v-col>
-                <v-col>
-                    <TextValueItem text="Дата начисления" value="01.01.2019"/>
-                </v-col>
-            </v-row>
-            <v-row>
-                <v-col>
-                    <TextValueItem text="Задолженность" value="314.35 р"/>
-                </v-col>
-                <v-col>
-                    <TextValueItem text="Пеня" value="314.35 р"/>
-                </v-col>
-                <v-col>
-                    <TextValueItem text="Дата оплаты" value="01.01.2019"/>
-                </v-col>
-            </v-row>
+            <v-expand-transition mode="out-in">
+                <div v-if="!showAllOperations">
+                    <v-row key="financial-control">
+                        <v-col cols="3">
+                            <v-text-field label="Месяц" hide-details outlined dense/>
+                        </v-col>
+                        <v-col cols="3" offset="6">
+                            <v-btn
+                                    block
+                                    color="primary lighten-1"
+                                    :disabled="showAllOperations"
+                                    @click="showAllOperations = true"
+                            >
+                                Показать все операции
+                            </v-btn>
+                        </v-col>
+                    </v-row>
+
+                    <v-row key="financial-short-1">
+                        <v-col>
+                            <TextValueItem text="Начисление" value="314.35 р"/>
+                        </v-col>
+                        <v-col>
+                            <TextValueItem text="Оплата" value="314.35 р"/>
+                        </v-col>
+                        <v-col>
+                            <TextValueItem text="Дата начисления" value="01.01.2019"/>
+                        </v-col>
+                    </v-row>
+                    <v-row key="financial-short-1">
+                        <v-col>
+                            <TextValueItem text="Задолженность" value="314.35 р"/>
+                        </v-col>
+                        <v-col>
+                            <TextValueItem text="Пеня" value="314.35 р"/>
+                        </v-col>
+                        <v-col>
+                            <TextValueItem text="Дата оплаты" value="01.01.2019"/>
+                        </v-col>
+                    </v-row>
+                </div>
+                <v-row v-else key="financial-table">
+                    <v-col>
+                        <FinancialList/>
+                    </v-col>
+                </v-row>
+            </v-expand-transition>
         </v-container>
     </v-card>
 </template>
@@ -38,15 +55,17 @@
 <script lang="ts">
     import { Component, Vue } from 'vue-property-decorator';
     import TextValueItem from '@/components/TextValueItem.vue';
+    import FinancialList from '@/components/contracts/contract-page/FinancialList.vue';
 
     @Component({
-    components: {
-        TextValueItem,
-    },
-})
-export default class FinancialCard extends Vue {
-
-}
+        components: {
+            TextValueItem,
+            FinancialList,
+        },
+    })
+    export default class FinancialCard extends Vue {
+        showAllOperations = false;
+    }
 </script>
 
 <style scoped lang="scss">
