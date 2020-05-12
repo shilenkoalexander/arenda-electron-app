e1c875f63cfcc70853a4c64f1141b01de4c1f7da
whine <sashazemzer@gmail.com>
2020-05-12 14:17:37 +0300

    добавлена обертка над методом db().queryFirstRow, которая возвращает Optional<DataObject>

--- src/backend/repository/repository.ts
+++ src/backend/repository/repository.ts
@@ -3,6 +3,14 @@ import db from 'better-sqlite3-helper';
 import { toLimit, toOrderBy } from '@/backend/utils/sql-util';
 import { OrderMapper } from '@/backend/mapper/order-mapper';
 import { ResultMapper } from '@/backend/mapper/result-mapper';
+import BetterSqlite3Helper from 'better-sqlite3-helper';
+import DataObject = BetterSqlite3Helper.DataObject;
+import Optional from '@/backend/utils/optional';
+
+
+export function findFirst(query: string): Optional<DataObject> {
+    return Optional.of(db().queryFirstRow(query));
+}
 
 export function queryWithPagination<T>(
     query: string,
@@ -10,10 +18,10 @@ export function queryWithPagination<T>(
     resultMapper: ResultMapper<T>,
     orderMapper: OrderMapper,
 ): Page<T> {
-    const count = db().queryFirstRow(`
+    const count = findFirst(`
         select count(*) as totalItems
         from (${query}) as q;
-    `) as any;
+    `).orElseThrowWithMessage(`База дала йобу и вернула undefined на count(*) запрос`);
 
     const items = db().query(`
         ${query}
--- src/views/contracts/ContractPage.vue
+++ src/views/contracts/ContractPage.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
         <v-tabs
-            v-model="tab"
-            background-color="primary lighten-1"
-            dark
+                v-model="tab"
+                background-color="primary lighten-1"
+                dark
         >
             <v-tab>Информация о договоре</v-tab>
             <v-tab>Информация об объектах</v-tab>
@@ -19,22 +19,22 @@
                             </v-col>
                             <v-col cols="5">
                                 <ContractInfoActionsCard
-                                    class="fill-height"
-                                    @recalculate="onRecalculate"
+                                        class="fill-height"
+                                        @recalculate="onRecalculate"
                                 />
                             </v-col>
                         </v-row>
                         <v-row justify="center">
                             <v-col cols="7">
                                 <FinanceCard
-                                    class="fill-height"
-                                    :contract-id="contractId"
-                                    :finance-periods="financePeriods"
-                                    :calculation-start-date="contractMainInfo.calculationStartDate"
-                                    :validity="contractMainInfo.validity"
-                                    :contract-extensions="contractExtensions"
-                                    @update-periods="updateFinancePeriods"
-                                    @update-extensions="updateContractExtensions"
+                                        class="fill-height"
+                                        :contract-id="contractId"
+                                        :finance-periods="financePeriods"
+                                        :calculation-start-date="contractMainInfo.calculationStartDate"
+                                        :validity="contractMainInfo.validity"
+                                        :contract-extensions="contractExtensions"
+                                        @update-periods="updateFinancePeriods"
+                                        @update-extensions="updateContractExtensions"
                                 />
                             </v-col>
                             <v-col cols="5">
@@ -59,73 +59,73 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import ContractInfo from '@/components/contracts/contract-page/ContractInfo.vue';
-import ContractStatusCard from '@/components/contracts/contract-page/ContractStatusCard.vue';
-import ContractInfoActionsCard from '@/components/contracts/contract-page/ContractInfoActionsCard.vue';
-import FinanceCard from '@/components/contracts/contract-page/finance/FinanceCard.vue';
-import ObjectDetailsCard from '@/components/contracts/contract-page/ObjectDetailsCard.vue';
-import RecalculatePeriodsDialog from '@/components/contracts/contract-page/dialogs/RecalculatePeriodsDialog.vue';
-import { getContractMainPageInfo, getFullContractExtensions } from '@/backend/repository/contract-repository';
-import { ContractPageMainInfo, FullContractExtension } from '@/backend/types/contract-types';
-import { FinancePeriod } from '@/backend/types/finance-types';
-import { getAllPeriods } from '@/backend/repository/finance-repository';
-import CenteredCard from '@/components/CenteredCard.vue';
-import logger from 'vuex/dist/logger';
+    import { Component, Vue } from 'vue-property-decorator';
+    import ContractInfo from '@/components/contracts/contract-page/ContractInfo.vue';
+    import ContractStatusCard from '@/components/contracts/contract-page/ContractStatusCard.vue';
+    import ContractInfoActionsCard from '@/components/contracts/contract-page/ContractInfoActionsCard.vue';
+    import FinanceCard from '@/components/contracts/contract-page/finance/FinanceCard.vue';
+    import ObjectDetailsCard from '@/components/contracts/contract-page/ObjectDetailsCard.vue';
+    import RecalculatePeriodsDialog from '@/components/contracts/contract-page/dialogs/RecalculatePeriodsDialog.vue';
+    import { getContractMainPageInfo, getFullContractExtensions } from '@/backend/repository/contract-repository';
+    import { ContractPageMainInfo, FullContractExtension } from '@/backend/types/contract-types';
+    import { FinancePeriod } from '@/backend/types/finance-types';
+    import { getAllPeriods } from '@/backend/repository/finance-repository';
+    import CenteredCard from '@/components/CenteredCard.vue';
 
-@Component({
-    components: {
-        CenteredCard,
-        RecalculatePeriodsDialog,
-        ContractInfo,
-        ContractStatusCard,
-        ContractInfoActionsCard,
-        FinanceCard,
-        ObjectDetailsCard,
-    },
-})
-export default class ContractPage extends Vue {
-    tab = null;
-    contractId: number | null = null;
-    contractMainInfo: ContractPageMainInfo | null = null;
-    financePeriods: FinancePeriod[] = [];
-    contractExtensions: FullContractExtension[] = [];
+    @Component({
+        components: {
+            CenteredCard,
+            RecalculatePeriodsDialog,
+            ContractInfo,
+            ContractStatusCard,
+            ContractInfoActionsCard,
+            FinanceCard,
+            ObjectDetailsCard,
+        },
+    })
+    export default class ContractPage extends Vue {
+        tab = null;
+        contractId: number | null = null;
+        contractMainInfo: ContractPageMainInfo | null = null;
+        financePeriods: FinancePeriod[] = [];
+        contractExtensions: FullContractExtension[] = [];
 
-    $refs!: {
-        recalculatePeriodDialog: RecalculatePeriodsDialog;
-    };
+        $refs!: {
+            recalculatePeriodDialog: RecalculatePeriodsDialog;
+        };
 
-    created() {
-        this.contractId = Number.parseInt(this.$route.params.id, 10);
-        this.contractMainInfo = getContractMainPageInfo(this.contractId);
-        this.updateAll();
-    }
-
-    onRecalculate() {
-        if (this.contractMainInfo && this.contractId) {
-            this.$refs.recalculatePeriodDialog.open(
-                this.contractId,
-                this.contractMainInfo.calculationStartDate,
-                this.contractMainInfo.validity,
-                this.contractExtensions,
-            );
+        created() {
+            this.contractId = Number.parseInt(this.$route.params.id, 10);
+            this.contractMainInfo = getContractMainPageInfo(this.contractId)
+                .orElseThrowWithMessage(`Отсутствует информация о договоре с id = ${this.contractId}`);
+            this.updateAll();
         }
-    }
 
-    updateFinancePeriods() {
-        this.financePeriods = getAllPeriods(this.contractId!);
-    }
+        onRecalculate() {
+            if (this.contractMainInfo && this.contractId) {
+                this.$refs.recalculatePeriodDialog.open(
+                    this.contractId,
+                    this.contractMainInfo.calculationStartDate,
+                    this.contractMainInfo.validity,
+                    this.contractExtensions,
+                );
+            }
+        }
 
-    updateContractExtensions() {
-        this.contractExtensions = getFullContractExtensions(this.contractId!);
-    }
+        updateFinancePeriods() {
+            this.financePeriods = getAllPeriods(this.contractId!);
+        }
 
-    updateAll() {
-        this.updateFinancePeriods();
-        this.updateContractExtensions();
-    }
+        updateContractExtensions() {
+            this.contractExtensions = getFullContractExtensions(this.contractId!);
+        }
 
-}
+        updateAll() {
+            this.updateFinancePeriods();
+            this.updateContractExtensions();
+        }
+
+    }
 </script>
 
 <style scoped lang="scss">
